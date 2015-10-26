60f1a6b2c14aa10a8504124487951145444da1ff
carl sednaoui <carlsednaoui@gmail.com>
2015-10-25 21:59:47 -0400

    Add double bar chart

--- plottable-implementation.js
+++ plottable-implementation.js
@@ -67,6 +67,52 @@ function plotScatterChart(selector) {
 };
 
 
+function plotDoubleBarChart(selector) {
+  var colorScale = new Plottable.Scales.Color();
+  var xScale = new Plottable.Scales.Linear();
+  var xAxis = new Plottable.Axes.Numeric(xScale, 'bottom');
+
+  var yScaleSenate = new Plottable.Scales.Linear();
+  var yAxisSenate = new Plottable.Axes.Numeric(yScaleSenate, 'left');
+  var plotSenate = new Plottable.Plots.Bar();
+
+  plotSenate.x(function(d) { return d.start_year; }, xScale);
+  plotSenate.y(function(d) {return d.democrats - d.republicans; }, yScaleSenate);
+  plotSenate.attr('fill', function(d) {return d.democrats - d.republicans > 0 ? 
+    "#0000FF" : "#FF0000"}, colorScale);
+  var labelSenate = new Plottable.Components.AxisLabel('Senate', -90);
+
+
+  var yScaleHouse = new Plottable.Scales.Linear();
+  var yAxisHouse = new Plottable.Axes.Numeric(yScaleHouse, 'left');
+  var plotHouse = new Plottable.Plots.Bar()
+    .x(function(d) { return d.start_year; }, xScale)
+    .y(function(d) { return d.democrats - d.republicans; }, yScaleHouse)
+    .attr('fill', function(d) {return d.democrats - d.republicans > 0 ?
+      "#0000FF" : "#FF0000"}, colorScale);
+  var labelHouse = new Plottable.Components.AxisLabel('House', -90);
+
+  var chart = new Plottable.Components.Table([
+    [labelSenate, yAxisSenate, plotSenate],
+    [labelHouse, yAxisHouse, plotHouse],
+    [null, null, xAxis]
+  ]);
+
+  chart.renderTo(selector);
+
+  // Load data for the Senate
+  $.get("/data/senate.json", function(data) {
+    plotSenate.addDataset(new Plottable.Dataset(data));
+  });
+
+  // Load data for the House
+  $.get("/data/house.json", function(data) {
+    plotHouse.addDataset(new Plottable.Dataset(data));
+  });
+};
+
+
 // Render charts
 plotLineChart('svg#line-chart');
 plotScatterChart('svg#scatterplot-chart');
+plotDoubleBarChart('svg#double-bar-chart');
