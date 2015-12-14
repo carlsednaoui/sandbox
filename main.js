b0e73c31011bfcc37f7c14791d6ae58ad99574d2
carl sednaoui <carlsednaoui@gmail.com>
2015-12-14 18:56:11 -0500

    Updates calendar

--- main.js
+++ main.js
@@ -2,11 +2,22 @@ console.log('sandbox is working!');
 
 // data comes from https://app.mailcharts.com/explore?q=dHlwZT1jb21wYW5pZXMmY29tcGFueUlkPTEwNTImY29tcGFueUlkPTEwNTMmY29tcGFueUlkPTIzOCZjb21wYW55SWQ9OTkmY29tcGFueUlkPTE4NDgmc3RhcnREYXRlPTIwMTUtMTAtMDEmZW5kRGF0ZT0yMDE1LTExLTAx
 $('#calendar').fullCalendar({
-  
-})
+  eventClick: function(event) {
+    if (event.url) {
+      window.open(event.url);
+      return false;
+    }
+  },
+  displayEventTime: false,
+  header: {
+    right:  'month, agendaWeek, agendaDay, prev,next'
+  },
+  firstDay: 1
+});
 
 var colors = ['#1DCE6D', '#2C83D1', '#00D3C5', '#A864C1', '#D19675'];
 var companyNames = [];
+var startDate;
 
 $.getJSON('/data/mc-data-for-calendar-report.json', function (data) {
   // Generate company list
@@ -18,12 +29,19 @@ $.getJSON('/data/mc-data-for-calendar-report.json', function (data) {
 
   $.each(data, function(i, email) {
     $('#calendar').fullCalendar('renderEvent', {
-      title: email.subject,
+      title: email.subject + ' - ' + email.companyName,
       start: moment(email.sentAt),
+      end: moment(email.sentAt).add('30', 'minutes'),
+      url: 'http://www.mailcharts.com/emails?guid=' + email.guid,
       color: colors[companyNames.indexOf(email.companyName)]
     }, true);
-  })
+  });
 
 });
+
+// TODO: Get this to work
+// $('#calendar').fullCalendar({
+//   defaultDate: startDate
+// });
 
 // $('#calendar').fullCalendar('renderEvent', {title: email.subject, start: moment(email.sentAt)}, true);
