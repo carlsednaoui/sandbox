e89486b40734c95d0c1b9959947a4f20adbe94c6
carl sednaoui <carlsednaoui@gmail.com>
2016-10-26 22:15:44 -0400

    Initial code for DS

---
+++ python/datascience-from-scratch/1-introduction.py
@@ -0,0 +1,47 @@
+from __future__ import division
+
+##########################
+#                        #
+# FINDING KEY CONNECTORS #
+#                        #
+##########################
+
+users = [
+    { "id": 0, "name": "Hero" },
+    { "id": 1, "name": "Dunn" },
+    { "id": 2, "name": "Sue" },
+    { "id": 3, "name": "Chi" },
+    { "id": 4, "name": "Thor" },
+    { "id": 5, "name": "Clive" },
+    { "id": 6, "name": "Hicks" },
+    { "id": 7, "name": "Devin" },
+    { "id": 8, "name": "Kate" },
+    { "id": 9, "name": "Klein" },
+    { "id": 10, "name": "Jen" }
+]
+
+friendships = [(0, 1), (0, 2), (1, 2), (1, 3), (2, 3), (3, 4),
+               (4, 5), (5, 6), (5, 7), (6, 8), (7, 8), (8, 9)]
+
+for user in users:
+  user["friends"] = []
+
+for i, j in friendships:
+  users[i]["friends"].append(users[j])
+  users[j]["friends"].append(users[i])
+
+# Print the friends name of the first user using list comprehension
+# https://docs.python.org/3.1/tutorial/datastructures.html#list-comprehensions
+print([x["name"] for x in users[0]["friends"]])
+
+def number_of_friends(user):
+  """how many friends does _user_ have"""
+  return len(user["friends"])
+
+total_connections = sum(number_of_friends(user) for user in users)
+num_users = len(users)
+avg_connections = total_connections / num_users
+
+print(avg_connections)
+
+# on page 5
